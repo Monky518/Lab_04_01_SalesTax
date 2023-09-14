a0253d57bf6b1baaceb47324f06f55a9fecb9da3
Monky518 <zoeyrizor@gmail.com>
2023-09-14 15:10:30 -0400

    Final Commit and Push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //Given the price of a purchase the program computes and outputs a 5% sales tax
+        //Be sure to use output statements that clearly display the price and the computed sales tax
+
+        double purchasePrice = 14;
+        double salesTax = 1.05;
+        double totalPrice;
+
+        totalPrice = purchasePrice * salesTax;
+        System.out.println("Your purchase price of " + purchasePrice + " with the sale tax at " + salesTax + " is " + totalPrice);
     }
 }
